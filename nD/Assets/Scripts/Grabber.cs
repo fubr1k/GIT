1e4d8269b594f7bdba4f7cac6cef21ef208088c1
jantak <jantak@metropolia.fi>
2016-03-22 00:47:03 +0200

    Added grabber, changed playertriggerstay

---
+++ nD/Assets/Scripts/Grabber.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Grabber : MonoBehaviour {
+
+	private bool _bgrabbed = false;
+
+	void Start () {
+	
+	}
+	
+	
+	void Update () {
+        
+        if (_bgrabbed && Input.GetKeyDown("f") && gameObject.transform.root.gameObject.tag == "Player")
+        {
+            transform.parent = null;
+            _bgrabbed = false;
+
+        }
+    }
+
+    void OnTriggerStay(Collider _col_)
+    {
+       
+        if (_col_.gameObject.tag == "Grabber" && Input.GetKeyDown("g") && !_bgrabbed)
+        {
+            gameObject.transform.parent = _col_.transform;
+            _bgrabbed = true;
+        }
+        
+    }
+}
